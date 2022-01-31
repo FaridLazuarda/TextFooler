2a5419d48631aea097dd3a4ba88a740b9fc32ccd
FaridLazuarda <faridadilazuarda29@gmail.com>
2022-01-31 21:09:34 +0700

    add: delete counter-fitted vectors

--- run_attack_classification.py
+++ run_attack_classification.py
@@ -14,7 +14,7 @@ command = 'python attack_classification.py --dataset_path data/yelp ' \
           '--target_model bert ' \
           '--target_model_path /scratch/jindi/adversary/BERT/results/yelp ' \
           '--max_seq_length 256 --batch_size 32 ' \
-          '--counter_fitting_embeddings_path /data/medg/misc/jindi/nlp/embeddings/counter-fitted-vectors.txt ' \
+        #   '--counter_fitting_embeddings_path /data/medg/misc/jindi/nlp/embeddings/counter-fitted-vectors.txt ' \
           '--counter_fitting_cos_sim_path /scratch/jindi/adversary/cos_sim_counter_fitting.npy ' \
           '--USE_cache_path /scratch/jindi/tf_cache'
 
